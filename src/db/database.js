c60e15c1d831ca6b5979a9698433ac078b7d915b
renan <renan_ota@hotmail.com>
2026-02-20 14:32:05 -0300

    Desfaz última alteração

--- src/db/database.js
+++ src/db/database.js
@@ -23,7 +23,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: true,
     ca: process.env.DB_CA_CERT,
   },
 
